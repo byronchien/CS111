bb3636fd05ec2a7bdaf473462025270ced8085fc
jasonsliu <jason.sean.liu@gmail.com>
2016-01-12 00:36:42 -0800

    Jason: Fixed booleans. Added cases for opening files.

--- Lab1a/simpsh.c
+++ Lab1a/simpsh.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 bool verboseflag = false;;
 
 int main (int argc, char **argv) {
   int c;
 
+  int offset;
   while (1)
     {
       static struct option long_options[] = {
@@ -23,18 +29,21 @@ int main (int argc, char **argv) {
 
       if (c == -1)
 	break;
-
+      
       switch(c)
 	{
 	case 'r':
 	  if(verboseflag){
 	    puts ("--rdonly option\n");
 	  }
+	  //  	  printf("%s, %s\n", optarg, argv[optind++]);
+	  printf("fd: %d\n", open(optarg, O_RDONLY));
 	  break;
 	case 'w':
 	  if(verboseflag){
 	    puts ("--wronly option\n");
 	  }
+	  printf("fd: %d\n", open(optarg, O_WRONLY));
 	  break;
 	case 'c':
 	  if(verboseflag){
@@ -45,7 +54,7 @@ int main (int argc, char **argv) {
 	  verboseflag = true;
 	  break;
 	default:
-	  break;
+	  abort();
 	}
     }
   return 0;
